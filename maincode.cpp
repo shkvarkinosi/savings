74c5a8554f676199d8daa6ff5e861fb376240e86
Henrik Lorna <162292178+shkvarkinosi@users.noreply.github.com>
2024-03-05 10:13:23 +0200

    Create maincode.cpp

---
+++ maincode.cpp
@@ -0,0 +1,20 @@
+bool TGAImage::read_tga_file(const std::string filename) {
+    std::ifstream in;
+    in.open(filename, std::ios::binary);
+    if (!in.is_open()) {
+        std::cerr << "can't open file " << filename << "\n";
+        return false;
+    }
+    TGAHeader header;
+    in.read(reinterpret_cast<char *>(&header), sizeof(header));
+    if (!in.good()) {
+        std::cerr << "an error occured while reading the header\n";
+        return false;
+    }
+    w   = header.width;
+    h   = header.height;
+    bpp = header.bitsperpixel>>3;
+    if (w<=0 || h<=0 || (bpp!=GRAYSCALE && bpp!=RGB && bpp!=RGBA)) {
+        std::cerr << "bad bpp (or width/height) value\n";
+        return false;
+    }
